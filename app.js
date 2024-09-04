9fd669a5ae2b307a9bde91c77f406d6eecfdcfd9
Àngel <angel@rom.cat>
2024-09-05 00:09:58 +0200

    COMMIT CORRECCIO CORS

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ import cors from "cors"
 
 const app = express()
 app.use(cors())
+//AFEGEIXO TOT AIXÒ
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+//FINS AQUÍ
 app.use(express.json())
 
 const port = process.env.PORT || 3401
